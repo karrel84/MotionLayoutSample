c3f821070e8e94ef0d321c560a41f86d88b67fd8
karrel <karrel84@naver.com>
2019-07-15 08:38:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/motionlayoutsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.motionlayoutsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.motion_02_basic)
+    }
+}
